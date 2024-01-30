317d7f21313ae2c18ef28f762660a286b741119d
Ilya <ilgorelov5@gmail.com>
2024-01-30 18:43:06 +0400

    Add button
    Add button thanks to which you can change N from N/M, where N is number of current position and M - number of all postions

---
+++ TopCreator/Assets/Scripts/changeActiveSecPos.cs
@@ -0,0 +1,13 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class changeActiveSecPos : MonoBehaviour
+{
+    [SerializeField] TopCreate topCreate;
+    [SerializeField] TMP_InputField inputField;
+    public void Change()
+    {
+        topCreate.activeSecondPosition = Int32.Parse(inputField.text);
+    }
+}
